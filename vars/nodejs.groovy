2095b17ca6c4d7777cfddb6428ce6438e43eba24
Raghu K <raghuk.vit@gmail.com>
2022-10-10 08:24:54 +0530

    Update roboshop-jenkins/

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -29,7 +29,7 @@ def call() {
         steps {
           sh '''
 env
-            sonar-scanner -Dsonar.host.url=http://172.31.94.112:9000 -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} -Dsonar.projectKey=${COMPONENT}
+            sonar-scanner -Dsonar.host.url=http://172.31.94.112:9000 -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} -Dsonar.projectKey=${COMPONENT} -Dsonar.qualitygate.wait=true
           '''
         }
       }
