1f7df7212be7cd8bb05fd20ce8d0c7a9bac60991
Raghu K <raghuk.vit@gmail.com>
2022-10-10 08:44:50 +0530

    Update roboshop-jenkins/

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,9 +1,11 @@
 def sonarCheck() {
   if (env.BRANCH_NAME == "main") {
     stage('Code Quality') {
-      sh '''
+      withCredentials([usernamePassword(credentialsId: 'SONAR', passwordVariable: 'SONAR_PSW', usernameVariable: 'SONAR_USR')]) {
+        sh '''
       sonar-scanner -Dsonar.host.url=http://172.31.94.112:9000 -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} -Dsonar.projectKey=${COMPONENT} -Dsonar.qualitygate.wait=true
     '''
+      }
     }
   }
 
